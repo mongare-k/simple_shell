a66cd21d52d4de7e154df97ecab5a12c45d6ea1c
Jedidah <jedidahwambuguw@gmail.com>
2022-12-22 01:50:25 +0300

    simple shell project

--- quote.c
+++ quote.c
@@ -1,25 +1,6 @@
 #include "quote.h"
 
 /**
- * quote_state - get the state associated with a given character
- * @c: character
- *
- * Return: the state associated with c
- */
-quote_state_t quote_state(char c)
-{
-	if (_isspace(c))
-		return (QUOTE_NONE);
-	if (c == '"')
-		return (QUOTE_DOUBLE);
-	if (c == '\'')
-		return (QUOTE_SINGLE);
-	if (c == '\\')
-		return (QUOTE_ESCAPE);
-	return (QUOTE_WORD);
-}
-
-/**
  * _quote_state_none - get state length and next state
  * @str: string
  * @state: state
@@ -39,30 +20,6 @@ size_t _quote_state_none(const char *str, quote_state_t *state)
 
 
 /**
- * quote_state_f - get the function associated with a given state
- * @s: state
- *
- * Return: the state associated with c
- */
-quote_state_fp quote_state_f(quote_state_t s)
-{
-	switch (s)
-	{
-	case QUOTE_NONE:
-		return (_quote_state_none);
-	case QUOTE_WORD:
-		return (_quote_state_word);
-	case QUOTE_DOUBLE:
-		return (_quote_state_double);
-	case QUOTE_SINGLE:
-		return (_quote_state_single);
-	case QUOTE_ESCAPE:
-		return (_quote_state_escape);
-	}
-	return (NULL);
-}
-
-/**
  * _quote_state_word - get state length and next state
  * @str: string
  * @state: state
@@ -80,18 +37,6 @@ size_t _quote_state_word(const char *str, quote_state_t *state)
 	return (len);
 }
 
-
-/**
- * quote_state_len - get the length of a given state
- * @str: string
- * @state: state
- *
- * Return: the state associated with c
- */
-size_t quote_state_len(const char *str, quote_state_t state)
-{
-	return (quote_state_f(state)(str, NULL));
-}
 
 /**
  * _quote_state_double - get state length and next state
